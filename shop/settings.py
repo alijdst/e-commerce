fbc78f4770933efc776e2e86a4224c6086a0d77d
ali jandardoost <110778634+alijdst@users.noreply.github.com>
2023-12-10 16:47:38 +0330

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -16,13 +16,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MERCHANT_CODE = 'asqduywhsajuhbagterfshgtrspofhyetsnv'
+MERCHANT_CODE = ''
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-56o3e@bjyj6h573rjk%su13a@#=aoue)f+f-hz&x^ogrk*+f^b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
