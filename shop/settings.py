6a6935af86c582384fe29d42a9148940436b7a60
ali jandardoost <110778634+alijdst@users.noreply.github.com>
2023-12-10 17:02:14 +0330

    Update settings.py

--- shop/settings.py
+++ shop/settings.py
@@ -22,7 +22,7 @@ MERCHANT_CODE = ''
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-56o3e@bjyj6h573rjk%su13a@#=aoue)f+f-hz&x^ogrk*+f^b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
